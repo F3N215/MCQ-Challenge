cd439231094fe8d4e8c253b1e624446a48c9043d
Daniel John <dcampo85@gmail.com>
2023-12-02 19:09:23 -0500

    testing question variable structure

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var questions = [
+    {
+        prompt: "What color is the book 1Q84 by Haruki Murakami?\(a) green\n(b) black\n\(c) white",
+        answer: "c"
+    },
+    {
+        prompt: "Who is Anton Chigurh?\(a) a policeman\n(b) an assassin\n\(c) a milkman",
+        answer: "b"
+    },
+    {
+        prompt: "In Neil Gaiman's 'Sandman' who are the 3 nightmares?\(a) Gorf, Hulpert & Brega\n(b) Corinthian, Fiddler's Green, & Gault\n\(c) Terry, Howard & Ryan",
+        answer: "b"
+    },
+]
