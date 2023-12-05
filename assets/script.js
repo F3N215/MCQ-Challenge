412468bf79f40020b8fb759ae901eb1f4f3ca56c
Daniel John <dcampo85@gmail.com>
2023-12-05 08:23:35 -0500

    added style, revisions to html

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,3 @@
-/* var score = 0; 
-for(var i=0; i < questions.length; i++){
-    var response = window.prompt(questions[i].prompt) 
-    if(response == questions[i].answer){ 
-        score++;
-        alert("You got it!")
-    } else {
-        alert("WRONGGG!");
-    }
-}
-alert("Your score was " + score + "/" + questions.length) */
-
 const questions = [
     {
         question: "What color is the book '1Q84' by Haruki Murakami?", 
@@ -31,11 +19,16 @@ const questions = [
         select: ["Michael", "Hubert", "Foggy", "Franklin"],
         answer: 3
     },
+    // more questions here
 ];
+
+let currentQuestion = 0;
+let correctAnswers = 0;
+
 
 function showQuestion() {
     const questionText = document.getElementById("question-text");
-    question.Text.textContent = questions[currentQuestion].question;
+    question.textContent = questions[currentQuestion].question;
 
     const choices = document.querySelectorAll(".select");
     choices.forEach((select, index) => {
@@ -45,6 +38,50 @@ function showQuestion() {
 
 
 }
+
+
+
+// submit functions to show results 
+    function showResults(questions, quizContainer, resultsContainer){
+        showQuestions(questions, quizContainer);
+        subtmitButton.onclick - function(){
+            showResults(questions, quizContainer, resultsContainer);
+        }
+       
+    showQuestions(questions, quizContainer);
+    submitBtn.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* PSEUDO
+- I want to trigger a set of questions when a user hits the start button
+- I want to loop through all questions in the array
+- I want to get the number of the questions answered correctly
+- When the first question is clicked I want a timer to start
+
+- I want to show a score of correct questions vs. incorrect questions (results)
+- I need a place to put the quiz, space for results, and a button
+- I need to store output and answer choices */
+
+
+
+
+
+
 
 /*
 function createQuiz(questions, quizContainer, resultsContainer, submitBtn){
@@ -74,31 +111,3 @@ function createQuiz(questions, quizContainer, resultsContainer, submitBtn){
     function showScore(questions, quizContainer, resultsContainer){
     }
 */
-
-// submit functions to show results 
-    function showResults(questions, quizContainer, resultsContainer){
-        showQuestions(questions, quizContainer);
-        subtmitButton.onclick - function(){
-            showResults(questions, quizContainer, resultsContainer);
-        }
-
-       
-    showQuestions(questions, quizContainer);
-    submitBtn.onclick = function(){
-        showResults(questions, quizContainer, resultsContainer);
-    }
-} 
-
-
-
-
-/*
-- I want to trigger a set of questions when a user hits the start button
-- I want to loop through all questions in the array
-- I want to get the number of the questions answered correctly
-- When the first question is clicked I want a timer to start
-
-
-- I want to show a score of correct questions vs. incorrect questions (results)
-- I need a place to put the quiz, space for results, and a button
-- I need to store output and answer choices */
